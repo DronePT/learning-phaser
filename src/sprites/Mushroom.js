ae3a49aeaf095515c6fac1ddadff335976460b07
Andre Mota <dronept@gmail.com>
2017-07-22 00:55:36 +0100

    add palyer deaths counter & reset method

--- src/sprites/Mushroom.js
+++ src/sprites/Mushroom.js
@@ -13,8 +13,43 @@ export default class extends Phaser.Sprite {
 
     // this.mushroom.body.bounce.y = 0.95
     this.body.collideWorldBounds = true
+    this.body.immovable = true
 
     this.lastMove = 0
+    this.deaths = 0
+
+    this.ripText = this.game.add.text(this.game.world.centerX, this.game.height/2, 'RiP')
+    this.ripText.font = 'Bangers'
+    this.ripText.padding.set(10, 16)
+    this.ripText.fontSize = 40
+    this.ripText.fill = '#0066cc'
+    this.ripText.smoothed = false
+    this.ripText.anchor.setTo(0.5)
+    this.ripText.visible = false
+    this.ripText.fixedToCamera = true
+    this.ripTextTimer = 0
+  }
+
+  resetPlayer () {
+    // this.game.camera.flash(0xff0000, 250)
+    this.game.camera.shake(0.02, 100)
+    this.x = this.game.world.centerX
+    this.y = this.game.world.height
+    this.deaths += 1
+
+    this.showGameOver()
+  }
+
+  showGameOver () {
+    clearTimeout(this.ripTextTimer)
+
+    this.ripText.text = `RIP #${this.deaths}`
+    this.ripText.visible = true
+
+    this.ripTextTimer = setTimeout(
+      () => { this.ripText.visible = false }
+      , 2000
+    )
   }
 
   move (x, y, isPressing = false) {
